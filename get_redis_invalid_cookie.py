f3cb1b3f6b32f5b0f2665c42f9a88b2fa6794a34
hnsqzyc <1017253325@qq.com>
2019-06-05 17:11:09 +0800

    00

--- get_redis_invalid_cookie.py
+++ get_redis_invalid_cookie.py
@@ -3,7 +3,7 @@ import redis
 r =redis.Redis(host="127.0.0.1", port=6379)
 #r =redis.Redis(host="47.105.103.8", port=56789, password="12345678")
 
-num = r.llen('invalid_ks_cookies')
+num = r.llen('ks_cookies')
 
 for i in range(num):
     res = r.rpoplpush('ks_cookies', 'ks_cookies').decode()
